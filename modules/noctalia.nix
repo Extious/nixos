74ebaae4e71b6f8f6416d48d20af3ba94681c620
zhaozhan <zhaozhan.site@gmail.com>
2025-12-23 14:26:10 +0800

    noctalia update

--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -3,4 +3,15 @@
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default 
     # ... 可能还有其他软件包
   ];
+  systemd.user.services.notalia-shell = {
+    description = "Noctalia Shell for Niri";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+    ExecStart = "${inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/noctalia-shell";
+    Restart = "always";
+    RestartSec = "2";
+    };
+  };
 }
+
