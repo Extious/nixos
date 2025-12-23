843876248e69a06ff0016646cbb25497b23264df
zhaozhan <zhaozhan.site@gmail.com>
2025-12-23 17:08:00 +0800

    feat: 简化 WeChat 安装方式，直接添加到系统包列表中

--- modules/programs.nix
+++ modules/programs.nix
@@ -22,14 +22,9 @@
     vscode
     clash-verge-rev
     qq
-    (pkgs.callPackage "${pkgs.path}/pkgs/by-name/we/wechat/linux.nix" {
-      pname = "wechat";
-      version = "4.1.0.13";
-      src = pkgs.fetchurl {
-        url = "https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.AppImage";
-        hash = "sha256-d/zdb69gmIcgAFCbWLKGfmD8ZFfuDlYdOy7vUJ7SiXc=";
-      };
-      meta = pkgs.wechat.meta;
-    })
+    wechat
  ];
+
+  # 修复 Electron/Chromium 应用在 Wayland 下的问题
+  environment.variables.NIXOS_OZONE_WL = "1";
 }
